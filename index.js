cb315e78a755d79b48225b2b6b7ea0a601dfb3ce
NenoPr <nenadpredan@gmail.com>
2022-12-13 16:37:59 +0100

    Used simple express instead.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const http = require("http");
+// const http = require("http");
 const fs = require("fs");
+const express = require("express");
+const app = express();
 
-const hostname = "127.0.0.1";
 const port = 8080;
 
 const indexPath = __dirname + "/index.html";
@@ -9,36 +10,52 @@ const aboutPath = __dirname + "/about.html";
 const contactPath = __dirname + "/contact-me.html";
 const _404Path = __dirname + "/404.html";
 
-const httpServer = http.createServer(httpHandler);
+// const httpServer = http.createServer(httpHandler);
 
-httpServer.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
 
-function httpHandler(req, res) {
-  console.log(__dirname + req.url);
-  if (req.url === "/") {
-    fs.readFile(indexPath, function (err, data) {
-      if (err == null) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        res.end();
-      }
-    });
-  }
-  fs.readFile(__dirname + req.url + '.html', function (err, data) {
-    if (err == null) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    } else {
-      fs.readFile(_404Path, function (err, data) {
-        if (err == null) {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.write(data);
-          res.end();
-        }
-      });
-    }
-  });
-}
+app.get("/", (req, res) => {
+  res.sendFile(indexPath);
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(aboutPath);
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(contactPath);
+});
+
+app.get('*', function(req, res){
+  res.sendFile(_404Path);
+});
+
+// function httpHandler(req, res) {
+//   console.log(__dirname + req.url);
+//   if (req.url === "/") {
+//     fs.readFile(indexPath, function (err, data) {
+//       if (err == null) {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.write(data);
+//         res.end();
+//       }
+//     });
+//   }
+//   fs.readFile(__dirname + req.url + '.html', function (err, data) {
+//     if (err == null) {
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write(data);
+//       res.end();
+//     } else {
+//       fs.readFile(_404Path, function (err, data) {
+//         if (err == null) {
+//           res.writeHead(200, { "Content-Type": "text/html" });
+//           res.write(data);
+//           res.end();
+//         }
+//       });
+//     }
+//   });
+// }
